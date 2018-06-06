03d377215c79824fa989b5e63c52cfd89678bc06
Spencer Goff <spencergoff1995@gmail.com>
2018-06-06 13:27:22 -0400

    Add files via upload

---
+++ firstNotRepeatingCharacter.py
@@ -0,0 +1,17 @@
+def firstNotRepeatingCharacter(s):
+    letterCount = {'a':0, 'b':0, 'c':0, 'd':0,'e':0, 'f':0,'g':0, 'h':0,'i':0, 'j':0,'k':0, 'l':0,'m':0, 'n':0,\
+    'o':0, 'p':0,'q':0, 'r':0,'s':0, 't':0,'u':0, 'v':0,'w':0, 'x':0,'y':0, 'z':0}
+    nonRepeatedCharacters = []
+    for i in range(len(s)):
+        testChar = s[i]
+        letterCount[testChar] += 1
+        if letterCount[testChar] == 1:
+            nonRepeatedCharacters.append(testChar)
+        elif letterCount[testChar] == 2:
+            nonRepeatedCharacters.remove(testChar)
+    if len(nonRepeatedCharacters) == 0:
+        nonRepeatedCharacters.append('_')
+    return nonRepeatedCharacters[0]
+
+s = 'abacabad'
+print(str(firstNotRepeatingCharacter(s)))
