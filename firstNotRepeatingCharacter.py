3b91b5e1a50647fa26a4db61fb5d0deeda7aae57
Spencer Goff <spencergoff1995@gmail.com>
2018-06-06 13:29:10 -0400

    Add files via upload

--- firstNotRepeatingCharacter.py
+++ firstNotRepeatingCharacter.py
@@ -1,3 +1,10 @@
+'''
+Given a string s, find and return the first instance of a non-repeating character in it.
+If there is no such character, return '_'.
+Note: Write a solution that only iterates over the string once and uses O(1) additional memory,
+since this is what you would be asked to do during a real interview.
+'''
+
 def firstNotRepeatingCharacter(s):
     letterCount = {'a':0, 'b':0, 'c':0, 'd':0,'e':0, 'f':0,'g':0, 'h':0,'i':0, 'j':0,'k':0, 'l':0,'m':0, 'n':0,\
     'o':0, 'p':0,'q':0, 'r':0,'s':0, 't':0,'u':0, 'v':0,'w':0, 'x':0,'y':0, 'z':0}
